dc5f9706ba9b1cea154bce18e420f2262944b7d6
estefanirup <estefaniaparecidaruproza@gmail.com>
2025-09-03 15:30:49 -0300

    get by id em person

--- appdemo/src/main/java/br/edu/utfpr/appdemo/controllers/PersonController.java
+++ appdemo/src/main/java/br/edu/utfpr/appdemo/controllers/PersonController.java
@@ -1,28 +1,54 @@
+
 package br.edu.utfpr.appdemo.controllers;
 
-import org.springframework.web.bind.annotation.RestController;
+import java.util.Optional;
+
+import org.apache.catalina.connector.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.edu.utfpr.appdemo.models.Person;
+import br.edu.utfpr.appdemo.repositories.PersonRepository;
+import jakarta.websocket.server.PathParam;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 
-
 @RestController
-@RequestMapping("person")
-
+@RequestMapping("/person")
 public class PersonController {
+    @Autowired
+    private PersonRepository personRepository;
+
     @GetMapping(value = {"", "/"})
     public String getPerson() {
-        return "Olá, eu sou uma pessoa e funciono!";
+        return "Oi, eu sou uma pessoa!";
     }
 
-    @PostMapping("")
-    public String create(@RequestBody String entity) {
+    @GetMapping("/{personId}")
+    public ResponseEntity <Person> getById(
+        @PathVariable String personId) {
+
+        var uuid = java.util.UUID.fromString(personId);
+        var result = personRepository.findById(uuid);
+
+        return result.isPresent() ?
+            ResponseEntity.ok(result.get()) :
+            ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    public Person create(@RequestBody Person entity) {
         System.out.println(entity);
-        
+        personRepository.save(entity);
         return entity;
     }
-
     
+
 }
